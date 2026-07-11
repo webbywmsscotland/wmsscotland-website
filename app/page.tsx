7e7bad3674d8c817740ee2a345d74b2ba9e437cf
Scott Webster <wmsscotland@icloud.com>
2026-07-11 16:27:42 +0100

    Add customer reviews section

--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,8 @@ export default function Home() {
 
       <Emergency />
 
+<Reviews />
+
       <WhyChoose />
 
       <RecentRepairs />
@@ -29,8 +31,7 @@ export default function Home() {
 
       <About />
 
-      <Reviews />
-
+    
       <Areas />
 
       <FAQ />
